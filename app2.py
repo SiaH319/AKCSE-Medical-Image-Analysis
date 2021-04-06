0ae1f4450e6965aaebe7b8bb6763ba50f4576536
sarah034 <78069463+sarah034@users.noreply.github.com>
2021-04-05 22:40:49 -0400

    hi

---
+++ app2.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("scraping.html")
+
+
+@app.route('/', methods=['POST'])
+def getvalue():
+    test = request.form['test']
+    print(test)
+    return render_template("pass.html", t=test)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
