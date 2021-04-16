954d7a8e3fb5b4787297754920ab21861e69e0f1
Rhina Kim <chihiroanihr@gmail.com>
2021-04-15 23:49:41 -0400

    Augmented Dataset added

--- format-dataset/image_augmentation.py
+++ format-dataset/image_augmentation.py
@@ -16,22 +16,26 @@ from torchvision.utils import save_image
 ############################### LOAD DATA FUNCTIONS ###############################
 def load_data_brain():
     # load image files from dataset-mask folder
-    DATA_PATH = "dataset-mask"
-    data_map = []
-    for img_dir in glob.iglob(DATA_PATH + "/**/*.png", recursive=True):
-        #print(img_dir)
-        data_map.append(img_dir)
+    MASKED_DATA_PATH = "../dataset-mask"
+    IMG_DATA_PATH = "../dataset-original"
+    masked_data_map = []
+    img_data_map = []
+    for masked_dir in os.listdir(MASKED_DATA_PATH):
+        masked_data_map.append(masked_dir)
+    for img_dir in os.listdir(IMG_DATA_PATH):
+        img_data_map.append(img_dir)
 
     # Make data frame
-    df = pd.DataFrame({"path": data_map[:]})
-    df_no_masks = df[~df['path'].str.contains("masks")] # extract all images that does not contain str "masks" in filepath
-    df_masks = df[df['path'].str.contains("masks")] # extract all images that contains str "masks" in filepath
+    df_no_masks = pd.DataFrame({"path": img_data_map[:]}) # extract all images that does not contain str "masks" in filepath
+    df_masks = pd.DataFrame({"path": masked_data_map[:]}) # extract all images that contains str "masks" in filepath
     #print(df_masks)
     #print(df_no_masks)
 
     # Data sorting
-    no_masks = sorted(df_no_masks["path"].values, key=lambda string: int(string.split('/')[2].strip("img")))
-    masks = sorted(df_masks["path"].values, key=lambda string: int(string.split('/')[2].strip("img")))
+    no_masks = sorted(df_no_masks["path"].values, key=lambda string: int(string.split('.')[0].strip("img")))
+    masks = sorted(df_masks["path"].values, key=lambda string: int(string.split('.')[0].strip("img")))
+    #print(no_masks)
+    #print(masks)
 
     # file number mismatch ckeck
     def file_mismatch_check(no_masks, masks):
@@ -39,11 +43,11 @@ def load_data_brain():
             #print(no_masks[i], masks[i])
 
             # If there's a mismatch while mapping two files from masked and no-mask images
-            if no_masks[i].split('/')[2].strip("img") != masks[i].split('/')[2].strip("img"):
-                no_masks_num = int(no_masks[i].split('/')[2].strip("img"))
-                no_masks_num_prev = int(no_masks[i-1].split('/')[2].strip("img"))
-                masks_num = int(masks[i].split('/')[2].strip("img"))
-                masks_num_prev = int(masks[i-1].split('/')[2].strip("img"))
+            if no_masks[i].split('.')[0].strip("img") != masks[i].split('.')[0].strip("img"):
+                no_masks_num = int(no_masks[i].split('.')[0].strip("img"))
+                no_masks_num_prev = int(no_masks[i-1].split('.')[0].strip("img"))
+                masks_num = int(masks[i].split('.')[0].strip("img"))
+                masks_num_prev = int(masks[i-1].split('.')[0].strip("img"))
                 # if has duplicated file
                 if no_masks_num - no_masks_num_prev < 1:
                     pop = no_masks.pop(i)
@@ -76,7 +80,6 @@ def load_data_brain():
           pd.DataFrame({"no_mask_path": no_masks,
                         "mask_path": masks}))
 
-    #print(final_df)
     return no_masks, masks
 ###################################################################################
 
@@ -93,8 +96,8 @@ class PyTorchImageDataset(Dataset):
         # Convert to PIL image
         image, mask = F.to_pil_image(image), F.to_pil_image(mask)
         # Resize
-        resize = transforms.Resize(size=(512, 512))
-        image, mask = resize(image), resize(mask)
+        #resize = transforms.Resize(size=(512, 512))
+        #image, mask = resize(image), resize(mask)
         # Random horizontal flipping
         if random.random() > 0.5:
             image, mask = F.hflip(image), F.hflip(mask)
@@ -108,30 +111,31 @@ class PyTorchImageDataset(Dataset):
         # Transform to tensor
         image, mask = F.to_tensor(image), F.to_tensor(mask)
         # Normalize tensor
-        image, mask = F.normalize(image, (0.5), (0.5)), F.normalize(mask, (0.5), (0.5))
+        #image, mask = F.normalize(image, (0.5), (0.5), (0.5)), F.normalize(mask, (0.5), (0.5), (0.5))
+        mask = F.normalize(mask, (0.5), (0.5), (0.5))
         return image, mask
 
     def original_transform(self, image, mask):
         # Convert to PIL image
         image, mask = F.to_pil_image(image), F.to_pil_image(mask)
         # Resize
-        resize = transforms.Resize(size=(512, 512))
-        image, mask = resize(image), resize(mask)
+        #resize = transforms.Resize(size=(512, 512))
+        #image, mask = resize(image), resize(mask)
         # Transform to tensor
         image, mask = F.to_tensor(image), F.to_tensor(mask)
         # Normalize tensor
-        image, mask = F.normalize(image, (0.5), (0.5)), F.normalize(mask, (0.5), (0.5))
+        #image, mask = F.normalize(image, (0.5), (0.5), (0.5)), F.normalize(mask, (0.5), (0.5), (0.5))
         return image, mask
 
     def __getitem__(self, index):
         image = plt.imread(self.image_paths[index])
         #image = Image.fromarray(image).convert('RGB')
-        image = np.asarray(image).astype(np.uint8)
-        #image = np.array(Image.fromarray((image * 255).astype(np.uint8)).resize((512, 512)).convert('RGB'))
+        image = np.asarray(image*225).astype(np.uint8)
+        #image = np.array(Image.fromarray((image).astype(np.uint8)).resize((512, 512)).convert('RGB'))
         mask = plt.imread(self.target_paths[index])
-        #mask = Image.fromarray(mask).convert('RGB')
+        #mask = Image.fromarray(mask).convert('RBG')
         mask = np.asarray(mask).astype(np.uint8)
-        #mask = np.array(Image.fromarray((mask * 255).astype(np.uint8)).resize((512, 512)).convert('RGB'))
+        #mask = np.array(Image.fromarray((mask).astype(np.uint8)).resize((512, 512)).convert('RGB'))
         if (self.augmentation == True):
             x, y = self.transform(image, mask)
         else:
@@ -151,6 +155,15 @@ class PyTorchImageDataset(Dataset):
 ################################### MAIN INPUTS ###################################
 # load all original and masked data
 no_mask_image_list, mask_image_list = load_data_brain()  # returns a list of original(no-mask) image path and masked images path
+no_mask_img_abs_path = []
+mask_img_abs_path = []
+for img in no_mask_image_list:
+    no_masked_dir = "../dataset-original/" + img
+    Image.open(no_masked_dir).convert('L').save(img)
+    no_mask_img_abs_path.append(no_masked_dir)
+for img in mask_image_list:
+    masked_dir = "../dataset-mask/" + img
+    mask_img_abs_path.append(masked_dir)
 
 # let’s initialize the dataset class and prepare the data loader.
 
@@ -164,14 +177,14 @@ def save_img(img_dataloader, masked_dataloader, index, augmented=None):
             os.makedirs(masked_dir)
 
     if augmented:
-        original_dir = 'dataset-final/img' + str(index) + augmented + '/original'
-        masked_dir = 'dataset-final/img' + str(index) + augmented + '/mask'
+        original_dir = '../dataset-final/img' + str(index) + augmented + '/original'
+        masked_dir = '../dataset-final/img' + str(index) + augmented + '/mask'
         check_dir_exists(original_dir, masked_dir)
         path_name_img = original_dir + '/img' + str(index) + augmented + ".png"
         path_name_masked = masked_dir + '/img' + str(index) + augmented + ".png"
     else:
-        original_dir = 'dataset-final/img' + str(index) + '/original'
-        masked_dir = 'dataset-final/img' + str(index) + '/mask'
+        original_dir = '../dataset-final/img' + str(index) + '/original'
+        masked_dir = '../dataset-final/img' + str(index) + '/mask'
         check_dir_exists(original_dir, masked_dir)
         path_name_img = original_dir + '/img' + str(index) + ".png"
         path_name_masked = masked_dir + '/img' + str(index) + ".png"
@@ -191,9 +204,17 @@ def show_img(img):
     plt.show()
 
 
-original_datasets = PyTorchImageDataset(image_paths=no_mask_image_list, target_paths=mask_image_list)
-augmented_datasets1 = PyTorchImageDataset(image_paths=no_mask_image_list, target_paths=mask_image_list, augmentation=True)
-augmented_datasets2 = PyTorchImageDataset(image_paths=no_mask_image_list, target_paths=mask_image_list, augmentation=True)
+original_datasets = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path)
+augmented_datasets1 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets2 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets3 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets4 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets5 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets6 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets7 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets8 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets9 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
+augmented_datasets10 = PyTorchImageDataset(image_paths=no_mask_img_abs_path, target_paths=mask_img_abs_path, augmentation=True)
 
 for i in range(len(original_datasets)):
     # Display Images
@@ -204,6 +225,38 @@ for i in range(len(original_datasets)):
     x2, y2 = augmented_datasets2[i]
     augmented_img_dataloader2 = DataLoader(dataset=x2, batch_size=1)
     augmented_masked_dataloader2 = DataLoader(dataset=y2, batch_size=1)
+
+    x3, y3 = augmented_datasets3[i]
+    augmented_img_dataloader3 = DataLoader(dataset=x3, batch_size=1)
+    augmented_masked_dataloader3 = DataLoader(dataset=y3, batch_size=1)
+
+    x4, y4 = augmented_datasets4[i]
+    augmented_img_dataloader4 = DataLoader(dataset=x4, batch_size=1)
+    augmented_masked_dataloader4 = DataLoader(dataset=y4, batch_size=1)
+
+    x5, y5 = augmented_datasets5[i]
+    augmented_img_dataloader5 = DataLoader(dataset=x5, batch_size=1)
+    augmented_masked_dataloader5 = DataLoader(dataset=y5, batch_size=1)
+
+    x6, y6 = augmented_datasets6[i]
+    augmented_img_dataloader6 = DataLoader(dataset=x6, batch_size=1)
+    augmented_masked_dataloader6 = DataLoader(dataset=y6, batch_size=1)
+
+    x7, y7 = augmented_datasets7[i]
+    augmented_img_dataloader7 = DataLoader(dataset=x7, batch_size=1)
+    augmented_masked_dataloader7 = DataLoader(dataset=y7, batch_size=1)
+
+    x8, y8 = augmented_datasets8[i]
+    augmented_img_dataloader8 = DataLoader(dataset=x8, batch_size=1)
+    augmented_masked_dataloader8 = DataLoader(dataset=y8, batch_size=1)
+
+    x9, y9 = augmented_datasets9[i]
+    augmented_img_dataloader9 = DataLoader(dataset=x9, batch_size=1)
+    augmented_masked_dataloader9 = DataLoader(dataset=y9, batch_size=1)
+
+    x10, y10 = augmented_datasets10[i]
+    augmented_img_dataloader10 = DataLoader(dataset=x10, batch_size=1)
+    augmented_masked_dataloader10 = DataLoader(dataset=y10, batch_size=1)
 
     xor, yor = original_datasets[i]
     original_img_dataloader = DataLoader(dataset=xor, batch_size=1)
@@ -217,6 +270,23 @@ for i in range(len(original_datasets)):
     # show_img(make_grid(augmented_img2))
     augmented_masked2 = iter(augmented_masked_dataloader2).next()
     # show_img(make_grid(augmented_masked2))
+    augmented_img3 = iter(augmented_img_dataloader3).next()
+    augmented_masked3 = iter(augmented_masked_dataloader3).next()
+    augmented_img4 = iter(augmented_img_dataloader4).next()
+    augmented_masked4 = iter(augmented_masked_dataloader4).next()
+    augmented_img5 = iter(augmented_img_dataloader5).next()
+    augmented_masked5 = iter(augmented_masked_dataloader5).next()
+    augmented_img6 = iter(augmented_img_dataloader6).next()
+    augmented_masked6 = iter(augmented_masked_dataloader6).next()
+    augmented_img7 = iter(augmented_img_dataloader7).next()
+    augmented_masked7 = iter(augmented_masked_dataloader7).next()
+    augmented_img8 = iter(augmented_img_dataloader8).next()
+    augmented_masked8 = iter(augmented_masked_dataloader8).next()
+    augmented_img9 = iter(augmented_img_dataloader9).next()
+    augmented_masked9 = iter(augmented_masked_dataloader9).next()
+    augmented_img10 = iter(augmented_img_dataloader10).next()
+    augmented_masked10 = iter(augmented_masked_dataloader10).next()
+
     original_img = iter(original_img_dataloader).next()
     # show_img(make_grid(original_img))
     original_masked = iter(original_masked_dataloader).next()
@@ -226,6 +296,14 @@ for i in range(len(original_datasets)):
     save_img(original_img, original_masked, index, "original")
     save_img(augmented_img1, augmented_masked1, index, "a1")
     save_img(augmented_img2, augmented_masked2, index, "a2")
+    save_img(augmented_img3, augmented_masked3, index, "a3")
+    save_img(augmented_img4, augmented_masked4, index, "a4")
+    save_img(augmented_img5, augmented_masked5, index, "a5")
+    save_img(augmented_img6, augmented_masked6, index, "a6")
+    save_img(augmented_img7, augmented_masked7, index, "a7")
+    save_img(augmented_img8, augmented_masked8, index, "a8")
+    save_img(augmented_img9, augmented_masked9, index, "a9")
+    save_img(augmented_img10, augmented_masked10, index, "a10")
 
 '''
 for i in range(len(original_datasets)):
