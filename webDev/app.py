165025c872245788ac798987c0e6bb7fb8554107
sarah034 <78069463+sarah034@users.noreply.github.com>
2021-05-13 00:58:15 +0900

    bootstrap - responsive layout added

--- webDev/app.py
+++ webDev/app.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, BatchNormalization, Flatten
 import numpy as np
 import os
+import jinja2
 
 curr = os.getcwd()
 
@@ -59,7 +60,7 @@ tumorModel.add(Dropout(0.5))
 tumorModel.add(Dense(4, activation='softmax'))
 tumorModel.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-tumorModel.load_weights(__location__+'/static/model/tumor_final_model.h5')
+#tumorModel.load_weights(__location__+'/static/model/tumor_final_model.h5')
 
 COUNT = 0
 app = Flask(__name__)
@@ -69,27 +70,26 @@ app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
 def mainPage():
     return render_template('index-main.html')
 
-
 @app.route('/members')
 def members():
-    return render_template('index-member.html')
+    return render_template('index-main.html')
 
 @app.route('/project/description')
 def projectDescription():
     return render_template('project-description.html')
 
-@app.route('/project/demo/brain-mapping')
+@app.route('/project/brain-mapping')
 def brainMappingDemo():
     return render_template('project-demo-brain-mapping.html')
 
 
-@app.route('/project/demo/brain-mapping/result', methods=['POST'])
+@app.route('/project/brain-mapping-result', methods=['POST'])
 def brainMappingDemoResult():
     global COUNT
     img = request.files['image']
 
-    img.save(__location__+'/static/{}.jpg'.format(COUNT))    
-    img_arr = cv2.imread(__location__+'/static/{}.jpg'.format(COUNT))
+    img.save(__location__+'/static/inputs/{}.jpg'.format(COUNT))
+    img_arr = cv2.imread(__location__+'/static/inputs/{}.jpg'.format(COUNT))
     img_arr = cv2.resize(img_arr, (128,128))
     img_arr = img_arr / 255.0
     img_arr = img_arr.reshape(1, 128,128,3)
@@ -101,17 +101,17 @@ def brainMappingDemoResult():
     COUNT += 1
     return render_template('project-demo-brain-mapping-result.html', data=preds)
 
-@app.route('/project/demo/tumor-detection')
+@app.route('/project/tumor-detection')
 def tumorDetectionDemo():
     return render_template('project-demo-tumor-detection.html')
 
-@app.route('/project/demo/tumor-detection/result', methods=['POST'])
+@app.route('/project/tumor-detection-result', methods=['POST'])
 def tumorDetectionDemoResult():
     global COUNT
     img = request.files['image']
 
-    img.save(__location__+'/static/{}.jpg'.format(COUNT))    
-    img_arr = cv2.imread(__location__+'/static/{}.jpg'.format(COUNT))
+    img.save(__location__+'/static/inputs/{}.jpg'.format(COUNT))
+    img_arr = cv2.imread(__location__+'/static/inputs/{}.jpg'.format(COUNT))
 
     img_arr = cv2.resize(img_arr, (128,128))
     img_arr = img_arr / 255.0
@@ -123,7 +123,6 @@ def tumorDetectionDemoResult():
     c = round(prediction[0,2]*100, 2)
     d = round(prediction[0,3]*100, 2)
 
-
     preds = np.array([a,b,c,d])
     COUNT += 1
     return render_template('project-demo-tumor-detection-result.html', tumorData=preds)
@@ -131,7 +130,7 @@ def tumorDetectionDemoResult():
 @app.route('/load_img')
 def load_img():
     global COUNT
-    return send_from_directory(__location__+'/static', "{}.jpg".format(COUNT-1))
+    return send_from_directory(__location__+'/static/inputs', "{}.jpg".format(COUNT-1))
 
 @app.route('/SiaHamGithub')
 def SiaHamGithub():
@@ -157,7 +156,7 @@ def JamesSungLinkedin():
 @app.route('/dataset1')
 def dataset1():
     return redirect("https://www.kaggle.com/navoneel/brain-mri-images-for-brain-tumor-detection")
-               
+
 @app.route('/dataset2')
 def dataset2():
     return redirect("https://www.robots.ox.ac.uk/~vgg/software/via/")
@@ -172,6 +171,3 @@ def dataset4():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
